1229a679f10a5727dc94e6dc71dc0966c8329567
jamalumpberger <nozaki.kn@gmail.com>
2014-10-14 15:39:12 -0400

    remainingGuesses

--- js/main.js
+++ js/main.js
@@ -1,31 +1,48 @@
-var wordLetters = ["N", "Y", "C", "D", "A"];
+var wordLetters = ["n", "y", "c", "d", "a"];
 var guessedLetters = ["_ ", "_ ", "_ ", "_ ", "_ "];
+var remainingGuesses = 10;
 
 function guessLetter(userLetter) 
 {
-    while (guessedLetters.indexOf("_ ") !==-1) 
+    while (guessedLetters.indexOf("_ ") !== -1 && remainingGuesses > 0)
     {
         userLetter = prompt("Please enter a letter in the alphabet", "A");
+        var letterFound = false;
         
         for (var i = 0; i < wordLetters.length; i++) 
         {
             
             var letterToCheck = wordLetters[i];
     
-            if (letterToCheck === userLetter) 
+            if (letterToCheck.toLowerCase() === userLetter.toLowerCase()) 
             {
-                alert("You correctly guessed a letter!");
-                guessedLetters[i] = letterToCheck;
-                $('#letters').html(guessedLetters);
+                // alert("You correctly guessed a letter!");
+                guessedLetters[i] = letterToCheck.toLowerCase();
+                // $('#letters').html(guessedLetters);
+                letterFound = true;
             }
             
         }
-        
+
+        if (letterFound) {
+            alert("You correctly guessed a letter!");
+            $('#letters').html(guessedLetters);
+        }       
+
+        else {
+            remainingGuesses--;
+            alert("Try again. You have " + remainingGuesses + " guesses left.");
+            $('#guesses').html("You have " + remainingGuesses + " guesses left.");
+        } 
     }
     
     if (guessedLetters.indexOf("_ ") === -1)
     {
         alert("You win!");
+    }
+
+    else if(remainingGuesses <= 0) {
+        alert("You lose.");
     }    
 }    
 
